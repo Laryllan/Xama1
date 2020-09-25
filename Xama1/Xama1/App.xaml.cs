bd5748346f2afbce467acba572a8b4dc4724e95c
Laryllan <kl83@gmx.de>
2020-09-25 21:30:15 +0200

    Projektdateien hinzufügen.

---
+++ Xama1/Xama1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xama1.Services;
+using Xama1.Views;
+
+namespace Xama1 {
+    public partial class App : Application {
+
+        public App() {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Xama1/Xama1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Xama1.Models;
+
+namespace Xama1.ViewModels {
+    public class ItemDetailViewModel : BaseViewModel {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null) {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
